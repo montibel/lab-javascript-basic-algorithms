0965ee00b434f5af9b1bbcb22b11aeec61add262
“Marcela <mpmontiela@gmail.com>
2022-10-05 16:26:40 +0200

    done

--- js/index.js
+++ js/index.js
@@ -8,5 +8,12 @@ console.log("The navigator's name is" + " " + hacker2);
 
 // Iteration 2: Conditionals
 
+const isLonger = () =>{
+let a = hacker1.length;
+let b = hacker2.length;
+if (a > b) console.log("The driver has the longest name, it has" + " " + (hacker1.length) + " " + "characters.");
+else if( b > a) console.log("It seems that the navigator has the longest name, it has" + " " + (hacker2.length) + " " + "characters.");
+else if (a===b) console.log ("Wow, you both have equally long names");
+}
+isLonger()
 
-// Iteration 3: Loops
