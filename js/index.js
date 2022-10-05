8fe8266deae6a002d43a7181020bce6672815ec0
“Marcela <mpmontiela@gmail.com>
2022-10-05 14:42:51 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 // Iteration 1: Names and Input
-//
+const hacker1= "Marcela";
+console.log( "The driver's name is" + " " + hacker1);
+
+const hacker2= "Ehab";
+console.log("The navigator's name is" + " " + hacker2)
+
+
 // Iteration 2: Conditionals
 
 
