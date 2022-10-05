05fbfb5f87f2d2134ebfe4908464b96a1db0c2bc
“Marcela <mpmontiela@gmail.com>
2022-10-05 14:48:06 +0200

    done

--- js/index.js
+++ js/index.js
@@ -3,8 +3,10 @@ const hacker1= "Marcela";
 console.log( "The driver's name is" + " " + hacker1);
 
 const hacker2= "Ehab";
-console.log("The navigator's name is" + " " + hacker2)
+console.log("The navigator's name is" + " " +node hacker2);
 
 
+// Iteration 2: Conditionals
 
 
+// Iteration 3: Loops
